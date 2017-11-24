dba727465a05e1755e9ddfb522ea0c5b82b17ddb
nach9 <nur.achdiansyah@gmail.com>
2017-11-24 10:37:58 +0700

    reinstall

--- tictactoe/screenWelcome.js
+++ tictactoe/screenWelcome.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test</Text>
+        <Text>Welcome</Text>
       </View>
     );
   }
