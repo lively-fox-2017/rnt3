6c6195f0ef79448d78016f8e392cba7b443f83b1
sisinduku <saptanto.sindu@gmail.com>
2017-11-24 11:49:24 +0700

    Merge yang bener

--- src/actions/BoardAction.js
+++ src/actions/BoardAction.js
@@ -27,26 +27,6 @@ export const changeValue = (row, col, value) => {
   for (let i = 0; i < 3; i++) {
     let column = [];
     for (let j = 0; j < 3; j++) {
-      column.push('')
-    }
-    initialValue.push(column);
-  }
-  initialValue[row][col] = (value[row][col] == 'X') ? 'O' : 'X'
-  return {
-    type: INITBOARD,
-    payload: {
-      initialValue
-    }
-  }
-}
-
-export const changeValue = (row, col, value) => {
-  let initialValue = []
-  let count = [5, 5]
-  let member = ['X', 'O']
-  for (let i = 0; i < 3; i++) {
-    let column = [];
-    for (let j = 0; j < 3; j++) {
       column.push('----')
     }
     initialValue.push(column);
