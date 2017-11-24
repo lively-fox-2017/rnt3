c700ef225926650991cee67e91535da5804a3245
hacktiv8 <hacktiv8@hacktiv8s-Mac-mini-2.local>
2017-11-24 10:20:55 +0700

    add board ugly screen and press handler

--- components/Board.js
+++ components/Board.js
@@ -4,14 +4,41 @@ import { View, Text, StyleSheet } from 'react-native';
 class Board extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      board: [['i', 'i', 'i'],
+              ['i', 'i', 'i'],
+              ['i', 'i', 'i']],
+    }
+  }
+
+  printBoard() {
+    return this.state.board.map((row, indexR) => {
+
+      return (
+        row.map((i, indexC) => {return (<Text key={`${indexR} ${indexC}`} onPress={this.pressOnBox(`${indexR} ${indexC}`)}>{`| ${i} |`}</Text>)})
+      );
+    })
+  }
+
+  pressOnBox(id) {
+    return () => {
+      this.setState({
+        board: this.state.board.map((row, indexR) => {
+          return row.map((cont, indexC) => {
+            if (`${indexR} ${indexC}` === id) {
+              return 'o';
+            }
+            return cont;
+          })
+        })
+      })
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          LOLO
-        </Text>
+        {this.printBoard()}
       </View>
     )
   }
@@ -23,6 +50,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
   },
 });
 
