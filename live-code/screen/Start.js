ea268268eca1e792bef03aa8ceb9d7009ca422f2
JainalArifin <zainalunpam05@gmail.com>
2017-11-24 10:04:22 +0700

    membuat screen untuk di hubungkan ke navigasi

---
+++ live-code/screen/Start.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+export default Start extends Component {
+  render () {
+    return (
+      <View>
+        <Text>
+        ini start
+        </Text>
+      </View>
+    )
+  }
+}
---
+++ live-code/screen/Welcome.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+export default Welcome extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Selamat datang di permainan tictac to<Text>
+      <View>
+    )
+  }
+}
