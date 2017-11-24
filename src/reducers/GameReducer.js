179a41a321b121632b1732924c241a51974025aa
sisinduku <saptanto.sindu@gmail.com>
2017-11-24 11:43:21 +0700

    Last commit~

--- src/reducers/GameReducer.js
+++ src/reducers/GameReducer.js
@@ -1,12 +1,14 @@
-import { INITBOARD } from '../actions/BoardAction'
+import { INITBOARD, CHANGEVALUE } from '../actions/BoardAction'
 
 const defaultState = {
-  boardElement: []
+  boardElement: [[], [], []]
 }
 
 export default (state=defaultState, action) => {
   switch (action.type) {
     case INITBOARD:
+      return {...state, boardElement: action.payload.initialValue}
+    case CHANGEVALUE:
       return {...state, boardElement: action.payload.initialValue}
     default:
       return state
